394d7a9f12eeac267fc26bb22de1529a8e7299bc
Kevin López <kevinL-R>
2020-08-25 18:50:28 -0600

    segundo commit

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def readJson(filename):
 repetir_bucle= True
 while repetir_bucle:
     comandos= input('ingrese un comando: ')
+    #comando cargar
     if re.search('CaRgar',comandos,re.IGNORECASE)!=None:
         match=re.search('CaRgar',comandos,re.IGNORECASE)
         s = match.start()
@@ -27,8 +28,29 @@ while repetir_bucle:
         print(filenames)
         for filename in filenames:
             readJson(filename)
+       #comando seleccionar  
+    print(filenames)   
+    select= re.findall('Seleccionar|Donde',comandos, re.IGNORECASE)
+    if select :
+        print("(prueba) existe seleccion o donde en el comando")
+    #comando maximo
+    if re.search('Maximo',comandos,re.IGNORECASE)!=None:
+        print('(prueba) maximo')
+    #comando minimo
+    if re.search('Minimo',comandos,re.IGNORECASE)!=None:
+        print('(prueba) minimo')
+    #comando suma
+    if re.search('Suma',comandos,re.IGNORECASE)!=None:
+        print('(prueba) suma')
 
-    salir= input('presione 1 si desea salir si no presione enter: ')
+    #comando cuenta
+    if re.search('Cuenta',comandos,re.IGNORECASE)!=None:
+        print('(prueba) cuenta')
+    #comando reportar
+    if re.search('Reportar',comandos,re.IGNORECASE)!=None:
+        print('(prueba) reportar')
+
+    salir= input('presione 1 si desea salir, si desea utilizar otro comando presione enter: ')
     if salir=='1':
         repetir_bucle= False
-    print(comandos)
+    
