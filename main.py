edaae6e3350b0b660dc404bc5b217270a08d86be
Kevin López <kevinL-R>
2020-08-27 16:43:40 -0600

    cuarto commit

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ while repetir_bucle:
         print(filenames)
         for filename in filenames:
             try:
-                file = open(filename + '.json')
+                file = open(filename)
                 datos = file.read()
                 data = json.loads(datos)
                 dict=data
@@ -29,26 +29,81 @@ while repetir_bucle:
        #comando seleccionar  
      
     if re.search('Seleccionar',comandos,re.IGNORECASE)!=None:
-        print('(prueba) seleccionar')
-        match=re.search('Seleccionar',comandos,re.IGNORECASE)
-        s = match.start()
+        match=re.search('Seleccionar',comandos,re.IGNORECASE)  #obtendra el match de la palabra seleccionar
+        match2=re.search('Donde',comandos,re.IGNORECASE)      #obtendra el match de la palabra donde
+        s = match.start()                               #con s e s2 y e 2 definira de donde a donde estan seleccionar y donde
         e = match.end()
-        comando2=comandos[s:e]
-        atributo= comandos.replace(comando2,"")
-        atributo_n=atributo.replace(" ", "")
-        atributos= atributo_n.strip()
-        atributos_lista=atributos.split(",")
-        for filename in filenames:
-            file = open(filename + '.json')
-            datos_s = file.read()
-            data_s = json.loads(datos_s)
-            dict=data_s
-            for elemento in dict:     
-                for attribute in atributos_lista:
-                    if elemento['nombre']=="kevin":
-                        print(f"{elemento[attribute]50}")
+        s2 = match2.start()
+        e2 = match2.end()
+        comando2=comandos[s:e]                          #obtendremos la palabra por medio de los indices 
+        comando_d=comandos[e2:e2]
+        parte_1=comandos[s:s2]
+        parte_d=comandos[e2+1:len(comandos)]     #dividiremos el string esta parte contiene desde el final de "donde" hasta el final de la cadena
+        x = re.search("nombre|edad|promedio|activo", parte_d)  #buscara si existe alguna de estas condiciones a buscar
+        sx=x.start()                                            # obtendremos de donde a donde estan segun el indice
+        ex=x.end()
+        atributo_d=parte_d[sx:ex]                       #se obtendra el atributo condicion
+        atributo_p2= parte_d.replace(atributo_d,"")          #quitaremos esa palabra de la segunda parte de la cadena
+        atributo_condi=atributo_p2.replace("=", "")  #atributo que define la condicion de donde ="" quitando el signo igual
+        atributo= parte_1.replace(comando2,"") #en la primera parte de la cadena quitaremos el seleccionar
+        atributo_n=atributo.replace(" ", "") #quitaremos los espacios en blanco
+        atributos= atributo_n.strip()     #se quitara algun espacio entre palabras
+        atributos_lista=atributos.split(",") #se dividira en una lista con el delimitador ,
+        z=re.findall("nombre|edad|promedio|activo", parte_d)
+        
+        if atributo_d == "nombre":
+            condicion1="nombre"
+        if atributo_d == "promedio":
+            condicion1="promedio"
+        if atributo_d == "activo":
+            condicion1="activo"
+        if atributo_d == "promedio":
+            condicion1="promedio"
+        if atributo_d =="edad":
+            condicion1="edad"
+        
+        
+        atributocondi= atributo_condi[1:len(atributo_condi)-1]
+        x = re.findall("\d", atributo_condi)
+        if x:
+            atributocondi=float(atributo_condi)
+        
+        if atributo_n =="*":        #si nuestra variable donde contiene los atributos a buscar tiene * seleccionara todos los atributos automaticamente
+            for filename in filenames:
+                file = open(filename)
+                datos_s = file.read()
+                data_s = json.loads(datos_s)            #imprimimos los atributos segun las condiciones establecidas
+                dict=data_s
+                for elemento in dict: 
+                    condicion_atributo=elemento.get(condicion1)
+                    condicion2=atributocondi
+                    if condicion_atributo==condicion2:  #si la condicion del atributo coincide con la entrada imprimira el atributo a buscar
+                        print("-------------------------------------")
+                        
+                        print("nombre: "+ f"{elemento['nombre']:5} \n"
+                            "edad: "+ f"{elemento['edad']:5} \n"
+                            "promedio: "+ f"{elemento['promedio']:5} \n" 
+                            "activo: "+ f"{elemento['activo']:5} \n")
+                        print("------------------------------------")
+                    
+        else:              
+            for filename in filenames:
+                file = open(filename)
+                datos_s = file.read()
+                data_s = json.loads(datos_s)            #imprimimos los atributos segun las condiciones establecidas
+                dict=data_s
+                for elemento in dict: 
+                    condicion_atributo=elemento.get(condicion1)
+                    condicion2=atributocondi
+                    
+                    if condicion_atributo==condicion2:  #si la condicion del atributo coincide con la entrada imprimira el atributo a buscar
+                        print("-------------------------------------")
+                        for attribute in atributos_lista:
+                            print(attribute +":"+ " " + f"{elemento[attribute]:5}")
+                        print("------------------------------------")
+                    
 
-    #subcomando Donde
+    
 
     if re.search('Maximo',comandos,re.IGNORECASE)!=None:
         print('(prueba) maximo')   
